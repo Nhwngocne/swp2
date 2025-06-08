5f00ae83233ddb80136d707f1def588743e698d8
vinhhien8 <hiennvse180433@fpt.edu.vn>
2025-06-08 14:27:21 +0700

    create package exception

---
+++ src/main/java/com/swp391/exception/ErrorCode.java
@@ -0,0 +1,23 @@
+package com.swp391.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+
+@Getter
+public enum ErrorCode {
+    USER_EXISTED(1001,"User already exists", HttpStatus.BAD_REQUEST),
+    ;
+
+    private final int code;
+    private final String message;
+    private HttpStatusCode statusCode;
+
+    ErrorCode(int code, String message, HttpStatusCode statusCode) {
+        this.code = code;
+        this.message = message;
+        this.statusCode = statusCode;
+    }
+
+
+}
---
+++ src/main/java/com/swp391/exception/GlobalExceptionHandler.java
@@ -0,0 +1,12 @@
+package com.swp391.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    //@ExceptionHandler(value = Exception.class)
+
+
+}
