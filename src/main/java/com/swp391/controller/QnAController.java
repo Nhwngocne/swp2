8d4272d265a746db1b91777e5fcf726dc17d571a
Vu-Sang-43 <sangvdse180455@fpt.edu.vn>
2025-06-22 17:39:02 +0700

    Create entity QnA ( Service , Repository, Controller, Mapper )

---
+++ src/main/java/com/swp391/controller/QnAController.java
@@ -0,0 +1,57 @@
+package com.swp391.controller;
+
+
+import com.swp391.dto.request.QnAAnswerRequest;
+import com.swp391.dto.request.QnAQuestionRequest;
+import com.swp391.dto.response.QnAResponse;
+import com.swp391.service.QnAService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/qna")
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class QnAController {
+
+    QnAService qnaService;
+
+    // MEMBER gửi câu hỏi
+    @PostMapping("/ask")
+    @PreAuthorize("hasRole('MEMBER')")
+    public ResponseEntity<QnAResponse> createQuestion(@RequestBody QnAQuestionRequest request) {
+        return ResponseEntity.ok(qnaService.createQuestion(request));
+    }
+
+    // STAFF trả lời câu hỏi
+    @PostMapping("/answer")
+    @PreAuthorize("hasRole('STAFF')")
+    public ResponseEntity<QnAResponse> answerQuestion(@RequestBody QnAAnswerRequest request) {
+        return ResponseEntity.ok(qnaService.answerQuestion(request));
+    }
+
+    // Tất cả user đều có thể xem danh sách câu hỏi đã được trả lời
+    @GetMapping("/answered")
+    public ResponseEntity<List<QnAResponse>> getAllAnswered() {
+        return ResponseEntity.ok(qnaService.getAllAnswered());
+    }
+
+    // STAFF có thể xem danh sách câu hỏi chưa được trả lời
+    @GetMapping("/pending")
+    @PreAuthorize("hasRole('STAFF')")
+    public ResponseEntity<List<QnAResponse>> getPendingQuestions() {
+        return ResponseEntity.ok(qnaService.getPendingQuestions());
+    }
+
+    // Lấy chi tiết câu hỏi theo ID
+    @GetMapping("/{id}")
+    public ResponseEntity<QnAResponse> getQnAById(@PathVariable int id) {
+        return ResponseEntity.ok(qnaService.getById(id));
+    }
+}
---
+++ src/main/java/com/swp391/dto/request/QnAQuestionRequest.java
@@ -0,0 +1,8 @@
+package com.swp391.dto.request;
+
+import lombok.Data;
+
+@Data
+public class QnAQuestionRequest {
+    private String question;
+}
